4de72dad087114deb4c49f1a42d81d02c63d077d
Ibrahim Hosseini <sehosseini@me.com>
2023-05-31 11:54:50 +0330

    Initial Commit

---
+++ NetworkLayerSample/NetworkLayerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerSampleApp.swift
+//  NetworkLayerSample
+//
+//  Created by Ibrahim on 5/31/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
